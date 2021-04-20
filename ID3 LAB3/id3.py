91b926905b4a3d46cf7d868a945b4be39ee5fe1b
JainJai2305 <53936875+JainJai2305@users.noreply.github.com>
2021-04-20 15:21:31 +0530

    Add files via upload

---
+++ ID3 LAB3/id3.py
@@ -0,0 +1,123 @@
+import pandas as pd
+import math
+import numpy as np
+import pprint
+
+
+data=pd.read_csv("/id3_dataset.csv")
+print("\n Input Data Set is:\n", data)
+features = [f for f in data]
+features.remove("answer")
+
+
+class Node:
+    def __init__(self):
+        self.children = []
+        self.value = ""
+        self.isLeaf = False
+        self.pred = ""
+
+def find_entropy(examples):
+    pos = 0.0
+    neg = 0.0
+    for _, row in examples.iterrows():
+        if row["answer"] == "yes":
+            pos += 1
+        else:
+            neg += 1
+    if pos == 0.0 or neg == 0.0:
+        return 0.0
+    else:
+        p = pos / (pos + neg)
+        n = neg / (pos + neg)
+        return -(p * math.log(p, 2) + n * math.log(n, 2))
+
+def info_gain(examples, attr):
+    uniq = np.unique(examples[attr])
+    gain = find_entropy(examples)
+    for u in uniq:
+        subdata = examples[examples[attr] == u]
+        sub_e = find_entropy(subdata)
+        gain -= (float(len(subdata)) / float(len(examples))) * sub_e
+    return gain
+
+
+def id3(examples, attrs):
+  root = Node()
+
+  max_gain = 0
+  max_feat = ""
+  for feature in attrs:
+      gain = info_gain(examples, feature)
+      if gain > max_gain:
+          max_gain = gain
+          max_feat = feature
+  root.value = max_feat
+  uniq = np.unique(examples[max_feat])
+  for u in uniq:
+      subdata = examples[examples[max_feat] == u]
+      if find_entropy(subdata) == 0.0:
+          newNode = Node()
+          newNode.isLeaf = True
+          newNode.value = u
+          newNode.pred = np.unique(subdata["answer"])
+          root.children.append(newNode)
+      else:
+          tempNode = Node()
+          tempNode.value = u
+          new_attrs = attrs.copy()
+          new_attrs.remove(max_feat)
+          child = id3(subdata, new_attrs)
+          tempNode.children.append(child)
+          root.children.append(tempNode)
+  return root
+
+
+def printTree(root: Node, depth=0):
+    for i in range(depth):
+        print("\t", end="")
+    print(root.value, end="")
+    if root.isLeaf:
+        print(" : ", root.pred)
+    print()
+    for child in root.children:
+        printTree(child, depth + 1)
+
+root = id3(data, features)
+print("Final decision tree:\n")
+printTree(root)
+
+
+
+#  Input Data Set is:
+#       outlook temperature humidity    wind answer
+# 0      sunny         hot     high    weak     no
+# 1      sunny         hot     high  strong     no
+# 2   overcast         hot     high    weak    yes
+# 3       rain        mild     high    weak    yes
+# 4       rain        cool   normal    weak    yes
+# 5       rain        cool   normal  strong     no
+# 6   overcast        cool   normal  strong    yes
+# 7      sunny        mild     high    weak     no
+# 8      sunny        cool   normal    weak    yes
+# 9       rain        mild   normal    weak    yes
+# 10     sunny        mild   normal  strong    yes
+# 11  overcast        mild     high  strong    yes
+# 12  overcast         hot   normal    weak    yes
+# 13      rain        mild     high  strong     no
+# Final decision tree:
+
+# outlook
+# 	overcast :  ['yes']
+
+# 	rain
+# 		wind
+# 			strong :  ['no']
+
+# 			weak :  ['yes']
+
+# 	sunny
+# 		humidity
+# 			high :  ['no']
+
+# 			normal :  ['yes']

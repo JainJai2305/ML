62136a864d1921d2ebb841d270028468152fc029
JainJai2305 <53936875+JainJai2305@users.noreply.github.com>
2021-06-15 09:56:54 +0530

    Update k_means.py

--- lab-6/k_means.py
+++ lab-6/k_means.py
@@ -1,12 +1,36 @@
-from sys import intern
-from openpyxl.workbook import workbook
-import requests
-from openpyxl import load_workbook
-from bs4 import BeautifulSoup
+import matplotlib.pyplot as plt
+from sklearn import datasets
+from sklearn.cluster import KMeans
+import sklearn.metrics as sm
+import pandas as pd
+import numpy as np
 
-book= load_workbook('internships.xlsx')
-ws=book.worksheets[0]
-ws_tables =[]
+iris = datasets.load_iris()
+X = pd.DataFrame(iris.data)
+X.columns = ['Sepal_Length','Sepal_Width','Petal_Length','Petal_Width']
 
-for i in range(2,10) :ws['A'+str(i)]="Number of openings"
-book.save('internships.xlsx')
+y = pd.DataFrame(iris.target)
+y.columns = ['Targets']
+
+print(X.head())
+print(y.head())
+
+model = KMeans(n_clusters=3)
+model.fit(X)
+plt.figure(figsize=(14,7))
+
+colormap = np.array(['red', 'lime', 'black'])
+# Plot the Original Classifications
+plt.subplot(1, 2, 1)
+plt.scatter(X.Petal_Length, X.Petal_Width, c=colormap[y.Targets], s=40)
+plt.title('Real Classification')
+plt.xlabel('Petal Length')
+plt.ylabel('Petal Width')
+# Plot the Models Classifications
+plt.subplot(1, 2, 2)
+plt.scatter(X.Petal_Length, X.Petal_Width, c=colormap[model.labels_], s=40)
+plt.title('K Mean Classification')
+plt.xlabel('Petal Length')
+plt.ylabel('Petal Width')
+print('The accuracy score of K-Mean: ',sm.accuracy_score(y, model.labels_))
+print('The Confusion matrixof K-Mean:\n ',sm.confusion_matrix(y, model.labels_))
